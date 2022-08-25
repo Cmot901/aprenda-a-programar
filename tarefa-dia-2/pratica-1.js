5f15a64629fbacb07daefbd74ffc86ad3c70c2c0
Pedro Cavalcanti <cavalcanti.mota120@gmail.com>
2022-08-25 19:42:33 -0300

    Desafio js 1

--- tarefa-dia-2/pratica-1.js
+++ tarefa-dia-2/pratica-1.js
@@ -4,3 +4,5 @@ profissao = "estudante"
 idade = 26
 local = "Noruega"
 console.log('\n',nome,'\n',email,'\n',profissao,'\n',idade,'\n',local);
+console.log("Olá",nome,"! Somos aqui da agência de turismo Viagem dos Sonhos. Estamos te escrevendo este email, pois acabamos de confirmar as compras das passagens aéreas para",local,". Confirma pra gente alguns dados? A sua idade é",idade,"você é" ,profissao,
+"e o seu email é",email,"? Ficamos no aguardo. Muito obrigado e boa viagem! :D")
